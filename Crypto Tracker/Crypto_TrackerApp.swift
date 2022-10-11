61f6c17c9c3d30d1bd41cf1b7cdf44ab53a79956
artemhrynenko <artemgrinenko8@gmail.com>
2022-10-11 15:23:33 +0200

    Initial Commit

---
+++ Crypto Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto Tracker
+//
+//  Created by Artem Hrynenko on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
